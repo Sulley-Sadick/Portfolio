0afa6b2e4945a910cd8b6af1aad0114e429a0f8d
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-23 02:52:32 +0000

    Changed url for all navLinks from relative urls to absolute, changed some styles in the styles.css. and modified the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: "/Portfolio/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "Production" ? "/Portfolio/" : "/",
   plugins: [tailwindcss()],
-});
+}));
