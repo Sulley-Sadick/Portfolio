4264306f3414be6f8850a1285f5e0e6fd0afcd58
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-29 16:32:08 +0000

    Change urls from absolute urls to relative urls in all the html files to make transition between pages work and created docs directory and placed dist directory into in order to use the docs directory as the folder on github pages

--- src/views/pageTransitionView.js
+++ src/views/pageTransitionView.js
@@ -3,8 +3,4 @@ import Swup from "swup";
 // create swup instance out of the class to make the swup functionality work(transition between pages).
 const swup = new Swup();
 
-swup.hooks.on("content:replace", () => {
-  console.log("content replaced");
-});
-
 export default Swup;
