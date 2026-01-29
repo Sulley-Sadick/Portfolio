a66fd02ced13d8cdd41d9203f3cc3698ead41f99
Sulley-Sadick <sulleysadick766@gmail.com>
2026-01-29 15:53:51 +0000

    Did re-configuration about files(about.html, contact.html and projects.html) and changed some code functionalities to make pages transitions to also work

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => ({
   base: mode === "production" ? "/Portfolio/" : "/",
   plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: "index.html",
+        about: "about.html",
+        projects: "projects.html",
+        contact: "contact.html",
+      },
+    },
+  },
 }));
